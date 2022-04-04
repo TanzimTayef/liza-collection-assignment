e20dca8a9dfb0c28b7eeb3ddd7a68b79b7fe33aa
TanzimTayef <tayef464@gmail.com>
2022-04-04 22:00:14 +0600

    start assignment 09 product reivews website with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Header from './components/Header/Header';
-
 function App() {
   return (
     <div className="App">
-      
+     <h1>Hello talwind</h1>
     </div>
   );
 }
