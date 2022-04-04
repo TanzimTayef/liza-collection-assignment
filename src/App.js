61ab6a96e70bc19ae075e569f305d6e1695d3b32
TanzimTayef <tayef464@gmail.com>
2022-04-05 02:36:16 +0600

    created home section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/HomePage/Home';
-import ReviewPage from './components/ReviewPage/ReviewPage';
 import Reviews from './components/Reviews/Reviews';
 function App() {
   return (
     <div className="App container mx-auto px-2">
-      {/* <Header />
+     <Header />
       <Home />
-      <Reviews /> */}
-      <ReviewPage/>
+      <Reviews /> 
+   
     </div>
   );
 }
