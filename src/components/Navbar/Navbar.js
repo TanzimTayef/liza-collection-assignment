3ab0a82e108445e5a501163da644084bb0e921a5
TanzimTayef <tayef464@gmail.com>
2022-04-05 16:45:50 +0600

    edited navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <nav className='mt-3'>
-            <ul className='md:flex justify-center font-bold uppercase'>
-                <li><Link className='ml-12' to="/">Home</Link></li>
-                <li><Link className='ml-12' to="/reviewsPage">Reviews</Link></li>
-                <li><Link className='ml-12' to="/dashboard">Dashboard</Link></li>
-                <li><Link className='ml-12' to="/blogs">Blogs</Link></li>
-                <li><Link className='ml-12' to="/about">About</Link></li>
+        <nav className='nav'>
+            <ul className='ul'>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/reviewsPage">Reviews</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><Link to="/blogs">Blogs</Link></li>
+                <li><Link to="/about">About</Link></li>
             </ul>
        </nav>
     );
